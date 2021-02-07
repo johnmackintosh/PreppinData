1ddeac6abb71db6d0c5bfc326bdb1a5282410776
John MacKintosh <johnmackintosh@users.noreply.github.com>
2021-02-07 12:45:44 +0000

    Create DT-shorter.R

---
+++ 2021-05/DT-shorter.R
@@ -0,0 +1,35 @@
+    library(data.table) 
+    library(here)
+    library(janitor)
+    library(dplyr)
+    setwd(here("2021-05"))
+    
+    DT <- clean_names(fread("Joined Dataset.csv"))
+    DT[, from_date := as.IDate(DT$from_date, format = "%d/%m/%Y")]
+    
+    # find latest account manager
+    
+    DT[,latest_date := max(from_date), by = .(client)][] # get latest date
+    
+    # on the latest date for each client, who was the AM and what was the client ID?
+    am_lookup <- DT[from_date == latest_date, .(account_manager,client_id), 
+                     by = .(client)][]
+    am_lookup <- unique(am_lookup)
+    
+    # join and update account manager, client_id and original from date
+    
+    DT[,client_id := am_lookup[.BY,(client), client_id, on = "client"],
+       by = client]
+    
+    DT[,account_manager := am_lookup[.BY , account_manager, on = "client"],
+         by = client]
+    
+    DT[,from_date := latest_date, by = .(client)][,latest_date := NULL][]
+    
+    output <- unique(DT)
+    # check results match the expected output
+    results <- clean_names(fread("results.csv")) # the sample output provided
+    results[, from_date := as.IDate(results$from_date, format = "%d/%m/%Y")]
+    
+    # should be 0
+    fsetdiff(results, output)
