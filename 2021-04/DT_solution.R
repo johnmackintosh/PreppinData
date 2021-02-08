8860db813613d3cf6c656529c3f7fb45a8fef386
John MacKintosh <johnmackintosh@users.noreply.github.com>
2021-02-08 22:42:56 +0000

    Update DT_solution.R
    Removed use of {splitstackshape}. Like Bane said to Batman, that was unnecessary

--- 2021-04/DT_solution.R
+++ 2021-04/DT_solution.R
@@ -1,10 +1,8 @@
 library(readxl)
 library(purrr)
 library(data.table)
-library(splitstackshape)
 library(here)
 setwd(here("2021-04"))
-
 
 wb <- "PD 2021 Wk 4 Input.xlsx"
 all_sheets <- wb %>%
@@ -19,30 +17,17 @@ data <-  setDT(map_dfr(datasheets,
                  ~ read_excel(wb, sheet = .x),
                  .id = "Store"))
 
+DT <- melt(data,id.vars = c('Store','Date'), variable.factor = FALSE)
+DT[, c("Customer_Type", "Product") := tstrsplit(`variable`, " - ", fixed = TRUE)][,variable := NULL]
 
-
-DT <- melt(data, id.vars = c('Store','Date'), variable.factor = FALSE)
-setnames(DT <- cSplit(DT, splitCols = "variable", sep = "-", direction = 'wide'),
-         old = c('variable_1', 'variable_2'), new = c('Customer_Type','Product'))
 DT[,Quarter := quarter(Date)][,Date := NULL][]
-
 DT[,Products_Sold := sum(value), by = .(Store, Quarter)]
 
-out2 <- unique(DT[,`:=`(value = NULL, Customer_Type = NULL, Product = NULL)])[]
+out2 <- unique(DT[, -c('value','Customer_Type', 'Product')])
 
-# create a join key
+# join with the targets, and calculate variance
+out2 <- targets[out2, on = c('Store','Quarter')][,variance := Products_Sold - Target][]
 
-out2[, joinkey := paste0(Store,Quarter,sep = '_')][]
-targets[, joinkey := paste0(Store,Quarter,sep = '_')][]
-
-out2 <- targets[out2, on = 'joinkey'][,':='(joinkey = NULL,
-                                  i.Store = NULL,
-                                  i.Quarter = NULL)]
-
-out2[,variance := Products_Sold - Target][]
-
-setorder(out2, Quarter, -variance) 
-
-out2[,rank := frank(-variance), by = Quarter][]
-
+setorder(out2, Quarter, -variance) #order by variance in descending order
+out2[,rank := frank(-variance), by = Quarter][]  # rank by variance in descending order
 fwrite(out2,"output2DT.csv")
