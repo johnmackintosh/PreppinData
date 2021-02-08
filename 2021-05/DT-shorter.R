6827b008bca271f10da21f6c6f640a265e12dc94
John MacKintosh <johnmackintosh@users.noreply.github.com>
2021-02-08 22:03:23 +0000

    Update DT-shorter.R
    join and update multiple columns at once

--- 2021-05/DT-shorter.R
+++ 2021-05/DT-shorter.R
@@ -1,34 +1,26 @@
-    library(data.table) 
-    library(here)
-    library(janitor)
-    setwd(here("2021-05"))
+library(data.table) 
+library(here)
+library(janitor)
     
-    DT <- clean_names(fread("Joined Dataset.csv"))
-    DT[, from_date := as.IDate(DT$from_date, format = "%d/%m/%Y")]
+setwd(here("2021-05"))
     
-    # find latest account manager
+DT <- clean_names(fread("Joined Dataset.csv"))
+DT[, from_date := as.IDate(DT$from_date, format = "%d/%m/%Y")]
+ 
+# on the latest date for each client, who was the AM /  what's their client ID?   
+DT[,latest_date := max(from_date), by = .(client)][]  # latest date by client
+am_lookup <- DT[from_date == latest_date, .(account_manager,client_id), 
+                by = .(client)][]
+am_lookup <- unique(am_lookup) # look-up of unique values for joining
     
-    DT[,latest_date := max(from_date), by = .(client)][] # get latest date
-    
-    # on the latest date for each client, who was the AM and what was the client ID?
-    am_lookup <- DT[from_date == latest_date, .(account_manager,client_id), 
-                     by = .(client)][]
-    am_lookup <- unique(am_lookup)
-    
-    # join and update account manager, client_id and original from date
-    
-    DT[am_lookup, client_id := i.client_id, on = "client"]
-    
-    DT[am_lookup, account_manager := i.account_manager, on = "client"]
-    
-    DT[,from_date := latest_date, by = .(client)][,latest_date := NULL][]
-    
-    output <- unique(DT)
-    # check results match the expected output
-    results <- clean_names(fread("results.csv")) # the sample output provided
-    results[, from_date := as.IDate(results$from_date, format = "%d/%m/%Y")]
-    
-    # should be 0
-    fsetdiff(results, output)
-    
-    outputs <- fwrite(output,"output.csv")
+# join and update account manager, client_id and original 'from date', then de-dupe
+DT[am_lookup, `:=` (client_id = i.client_id,
+                   account_manager = i.account_manager), on = 'client']
+DT[,from_date := latest_date, by = .(client)][,latest_date := NULL][]
+DT <- unique(DT)
+
+# check results match the expected output, and write out to file
+results <- clean_names(fread("results.csv")) # the sample output provided
+results[, from_date := as.IDate(results$from_date, format = "%d/%m/%Y")]
+fsetdiff(results, DT) # should be 0
+outputs <- fwrite(DT,"output.csv") # all good, write the output
