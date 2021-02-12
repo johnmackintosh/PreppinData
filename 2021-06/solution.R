61dac8f4817b1e42950ffaef3f0d94987040406c
John MacKintosh <johnmackintosh@users.noreply.github.com>
2021-02-12 22:58:41 +0000

    Update solution.R

--- 2021-06/solution.R
+++ 2021-06/solution.R
@@ -1,31 +1,31 @@
 library(readxl)
 library(data.table)
+library(janitor)
+
 data <- read_excel("2021-06/PGALPGAMoney2019.xlsx", 
-        col_types = c("text", "numeric", "numeric", "text"))
+        col_types = c("text", "numeric", "numeric", "text")) %>% 
+    janitor::clean_names()
 
 DT <- setDT(copy(data))
-oldnames <- names(DT)
-newnames = c('player', 'money', 'events', 'tour')
-setnames(DT, old = oldnames, new = newnames)
-
-DT[, count := .N, by = player][]
+DT[,count := as.double(1L)][]
 DT[,`:=`(Total_Prize_Money = sum(money),
          Number_of_Players = sum(count),
          Number_of_Events = sum(events),
-         tour_rank = frank(-money)), by = tour][]
+         tour_rank = frank(- money)), by = tour]
+DT[,avg_per_event := money / events, by = player_name]
+DT[,total_rank := frank(- money)]
+DT[,Avg_Money_Per_Event := mean(avg_per_event), by = tour]
+DT[,rank_variance := total_rank - tour_rank, by = player_name]
+DT[,Avg_Difference_in_Ranking := mean(rank_variance), by = tour]
 
-DT[,avg_per_event := money / events, by = player]
-DT[, total_rank := frank(-money)]
-DT[,Avg_Money_Per_Event := mean(avg_per_event), by = tour][]
+keepnames <- c('tour','Total_Prize_Money', 'Number_of_Players', 
+'Number_of_Events', 'Avg_Money_Per_Event', 'Avg_Difference_in_Ranking')
 
-DT[,rank_variance := total_rank - tour_rank, by = player][]
-DT[,Avg_Difference_in_Ranking := mean(rank_variance), by = tour][]
+measurevars <- keepnames[!keepnames %chin% 'tour']
 
-DT <- unique(DT[,-c('player','money', 'events','count','avg_per_event',
-              'tour_rank','total_rank','rank_variance')])[]
-
-output <- melt(DT)
-output <- dcast(output, variable ~ tour) # figuring this out was the hardest part
-setcolorder(output,c('variable', 'PGA', 'LPGA')) 
-output[,difference_between_tours := LPGA - PGA]
-output[order(-variable)][]
+DT <- unique(DT[,..keepnames])
+DT <- melt(DT, id.vars = 'tour', measure.vars = measurevars)
+DT <- dcast(DT, variable ~ tour) 
+setcolorder(DT,c('variable', 'PGA', 'LPGA')) 
+DT[,difference_between_tours := LPGA - PGA]
+DT[order(-variable)][]
