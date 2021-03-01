0875bd3467cb4a8c20f4ac9523395af23cef6b4a
John MacKintosh <johnmackintosh@users.noreply.github.com>
2021-03-01 21:35:48 +0000

    Update solution.R

--- 2021-08/solution.R
+++ 2021-08/solution.R
@@ -5,36 +5,33 @@ library(here)
 
 setwd(here("2021-08"))
 
-choices <- read_excel("karaoke.xlsx", col_types = c("date","text", "text"))
+choices <- setDT(read_excel("karaoke.xlsx", 
+                            col_types = c("date","text", "text")))
 
-customers <- read_excel("karaoke.xlsx", sheet = "Customers", 
-                        col_types = c("text", "date"))
-
-setDT(choices)
-setDT(customers)
+customers <- setDT(read_excel("karaoke.xlsx", sheet = "Customers", 
+                        col_types = c("text", "date")))
 
 # create columns to join on, then sort
-choices[,`:=` (join_time = Date)][]
-customers[,`:=` (join_time = `Entry Time`)][]
+choices[,`:=` (join_time = Date)]; setkey(choices, join_time)
+customers[,`:=` (join_time = `Entry Time`)];setkey(customers, join_time)
 
-setkey(choices, join_time)
-setkey(customers, join_time)
 
 # create session number and cols to check that entry time within 10 mins of session start
-choices[,lag_date := shift(Date, type = 'lag', fill = 1L)]
-choices[,new_session := difftime(Date,lag_date,'mins')>= 59]
-choices[,session_number := cumsum(new_session == TRUE)]
-choices[,song_number := rowid(session_number)]
-choices[,session_start := min(Date), by = session_number]
-choices[,entry_check := session_start - lubridate::minutes(10)]
+choices[,lag_date := shift(Date, type = 'lag', fill = 1L)
+        ][,new_session := difftime(Date,lag_date,'mins')>= 59
+          ][,session_number := cumsum(new_session == TRUE)
+            ][,song_number := rowid(session_number)
+              ][,session_start := min(Date), by = session_number
+                ][,entry_check := session_start - lubridate::minutes(10)
+                  ][,`:=`(new_session = NULL, lag_date = NULL)]
 
-choices[,`:=`(new_session = NULL, lag_date = NULL)]
+merged <- customers[choices, roll = TRUE]
+merged[!between(`Entry Time`,entry_check,session_start), `Customer ID` := NA]
 
-test <- customers[choices, roll = TRUE]
-test[!between(`Entry Time`,entry_check,session_start), `Customer ID` := NA]
+setkey(merged,'Date','session_number','Customer ID','song_number')
 
-my_output <- test[,c('session_number','Customer ID', 'song_number','Date','Artist','Song')]
+my_output <- fwrite(merged[,c('session_number','Customer ID', 'song_number','Date','Artist','Song')],"final_output.csv")
 
-setkey(test,'Date','session_number','Customer ID','song_number')
 
-fwrite(my_output,"final_output.csv")
+
+
