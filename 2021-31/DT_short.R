05ea1563f769eaa94885c403974342f9e7bc41f0
John MacKintosh <johnmackintosh@users.noreply.github.com>
2021-08-08 23:23:03 +0100

    Update DT_short.R

--- 2021-31/DT_short.R
+++ 2021-31/DT_short.R
@@ -2,9 +2,9 @@ library(here)
 library(data.table)
 setwd(here("2021-31"))
 
-DT <- fread(cmd = paste("grep", " Sold ", "Input.csv"), drop = c(1,4)) # drop Date and Status cols
-setnames(DT, new = c('Store', 'Item', 'Sales'))
-DT <- dcast(DT, Store ~ Item, value.var = 'Sales', fun.aggregate = sum)
-DT[,'Items Sold per Store' := rowSums(.SD), by = Store]
-DT[,.(`Items Sold per Store`, Wheels, Tyres, Saddles, Brakes, Store)][]
-fwrite(DT,"DT.csv")
+DT <- fread(cmd = paste("grep", " Sold ", "Input.csv"), drop = c(1,4),
+            col.names = c("Store","Item","Sales")) # drop Date & Status, rename Sales
+DT <- dcast(DT, Store ~ Item, value.var = 'Sales', fun.aggregate = sum) # PIVOT!
+DT[,'Items Sold per Store' := rowSums(.SD), by = Store] # Totals by Store
+DT[,.(`Items Sold per Store`, Wheels, Tyres, Saddles, Brakes, Store)][] # or use setcolorder()
+fwrite(DT,"DT2.csv")
